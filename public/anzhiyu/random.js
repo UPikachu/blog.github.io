c16167f62380532e1c72b1de081ac137c1d520ec
UPikachu <2810546658@qq.com>
2025-09-08 20:12:22 +0800

    更新

--- public/anzhiyu/random.js
+++
@@ -1,3 +0,0 @@
-var posts=["posts/4a17b156/","posts/7fefb892/","posts/74fda535/","posts/3bf4a27/"];function toRandomPost(){
-    pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
-  };
